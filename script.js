79dcbba560002a670aa5272fd7700f2afe0129d7
Atikam Anudeep <107506463+anudeepatikam@users.noreply.github.com>
2023-06-02 07:58:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+   
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+       $('header').toggleClass('toggle'); 
+    });
+  
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle'); 
+
+        if($(window).scrollTop() >0){
+            $('.top').show();
+        }
+        else{
+            $('.top').hide(); 
+        }
+    }); 
+    // smooth scroling 
+    $('a[href*="#"]').on('click',function(e){
+             e.preventDefault();
+             $('html, body').animate({
+                scrollTop:$($(this).attr('href')).offset().top,
+
+             },
+             500,
+             'linear'
+             );
+    });
+});
+const downloadBtn=document.querySelector(".download-btn");
+const fileLink="file:///C:/Users/atika/OneDrive/Desktop/resume/Chanti%20resume-2.pdf";
+const initTimer=()=>{
+        let timer=downloadBtn.CDATA_SECTION_NODE.timer;
+        downloadBtn.classList.add("timer");
+       
+        const initCounter=setInterval(()=>{
+            
+            clearInterval(initCounter);
+            location.href=fileLink;
+           
+           
+        }
+        ,1000);
+};
+downloadBtn.addEventListener("click",initTimer);
